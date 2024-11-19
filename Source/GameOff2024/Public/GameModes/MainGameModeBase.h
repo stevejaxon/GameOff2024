878661bd6daeee6511618fd1eadfe7b2095fdd1a
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2024-11-19 15:41:53 -0500

    feat: added HUD widget

--- Source/GameOff2024/Public/GameModes/MainGameModeBase.h
+++ Source/GameOff2024/Public/GameModes/MainGameModeBase.h
@@ -25,4 +25,6 @@ public:
 	void StoreTileRef(ATileBase&& Tile, int Index);
 	void TileSpawningCompleted();
 	FTileGridConfiguration* GridSpawningConfig();
+
+	UFUNCTION(BlueprintImplementableEvent) void OnLoadNextLevel();
 };
